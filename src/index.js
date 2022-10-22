288654cb26c1526d06d96c892d46eef2279034cf
yulia-bisyuk <yulia.bisyuk@gmail.com>
2022-10-22 23:43:37 +0300

    feat: line width and line color pickers added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 const canvas = document.getElementById('canvas');
 const button = document.getElementById('collapse');
+const inputColor = document.getElementById('color');
+const inputLineWidth = document.getElementById('lineWidth');
 const ctx = canvas.getContext('2d');
 
 let coord = { x: 0, y: 0 };
+let color = null;
+let lineWidth = 5;
 
 document.addEventListener('mousedown', start);
+
+inputColor.addEventListener('input', onColorInputChange);
+inputLineWidth.addEventListener('change', onLineWidthChange);
+
+function onColorInputChange(event) {
+  console.log('onInput', event.target.value);
+  color = event.target.value;
+}
+
+function onLineWidthChange(event) {
+  lineWidth = event.target.value;
+  console.log('onLineWidthChange', event.target.value);
+}
 
 function setPosition(event) {
   coord.x = event.clientX - canvas.offsetLeft;
@@ -12,30 +29,36 @@ function setPosition(event) {
 }
 
 function start(event) {
-  console.log('start');
   if (event.target.id !== 'canvas') return;
+  console.log('start');
+
   setPosition(event);
   document.addEventListener('mousemove', draw);
 }
 
 function draw(event) {
+  if (event.target.id !== 'canvas') return;
   console.log('draw');
   document.removeEventListener('mousedown', start);
   ctx.beginPath();
-  ctx.lineWidth = 1;
-  ctx.strokeStyle = '#000';
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = color;
   ctx.moveTo(coord.x, coord.y);
+  ctx.stroke();
   setPosition(event);
   ctx.lineTo(coord.x, coord.y);
+  ctx.lineCap = 'round';
   ctx.stroke();
   document.addEventListener('mouseup', stop);
 }
 
 function stop(event) {
+  if (event.target.id !== 'canvas') return;
   console.log('stop');
   document.removeEventListener('mousemove', draw);
 
   setPosition(event);
+  color = null;
   document.removeEventListener('mouseup', stop);
   document.addEventListener('mousedown', start);
 }
